cbda415b99cbcff4afa78639a0e7bbca9734ed69
sebas1017 <henaoerazosebastian995@gmail.com>
2022-05-12 13:36:26 -0500

    implementacion endpoint all

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-from argparse import _UNRECOGNIZED_ARGS_ATTR
 from fastapi import FastAPI, Response, Depends
 from psycopg2 import OperationalError
-from bs4 import BeautifulSoup
 from core.config import settings
 from sqlalchemy.orm import Session
 from db.session import get_db, engine 
 from db.base import Base  
-from db.models.statistics import StatisticsCountries, Vehicles
+from db.models.statistics import Vehicles
+from db.models.serializer import VehicleAvailable
+from fastapi.encoders import jsonable_encoder
+
 import json
 import requests
 import pandas as pd
@@ -46,6 +47,8 @@ async def datos_delegaciones(response:Response):
 	datos = obtener_datos_delegaciones()
 	print(datos)
 	return ""
+
+@app.get("/api/v1/all_vehicles/")
 async def index(response:Response, db: Session = Depends(get_db)):
 	url = "https://datos.cdmx.gob.mx/api/3/action/datastore_search?resource_id=ad360a0e-b42f-482c-af12-1fd72140032e"	
 	response = requests.get(url)
@@ -54,13 +57,27 @@ async def index(response:Response, db: Session = Depends(get_db)):
 		units_availables =  response.json()["result"]["records"]
 		for unit in units_availables:
 			if unit["vehicle_current_status"] == 1:
-				units_final_availables.append(unit)
-		data = db.query(Vehicles).all()
-		if len(data) < len(units_final_availables):
-			units_final_availables = units_final_availables[len(data)-1 :]
-			insert_data(db, units_final_availables)
-		response.status_code == 200
-		return {"message":"Proceso realizado correctamente"}
+				data = {
+				"vehicle_id":unit["vehicle_current_status"],
+				"vehicle_label":unit["vehicle_label"],
+				"vehicle_current_status":unit["vehicle_current_status"],
+				"position_latitude":unit["position_latitude"],
+				"position_longitude":unit["position_longitude"],
+				"geographic_point":unit["geographic_point"],
+				"position_speed":unit["position_speed"],
+				"position_odometer":unit["position_odometer"],
+				"trip_schedule_relationship":unit["trip_schedule_relationship"],
+				"trip_id":unit["trip_id"],
+				"trip_start_date":unit["trip_start_date"],
+				"trip_route_id":unit["trip_route_id"]
+				}
+				units_final_availables.append(data)
+		print( dir ( db.query(Vehicles)))
+
+		return units_final_availables
+		
+			
+
 	else:
 		response.status_code == 500
 		return {"message":"Error en API externa de datos de metrobus recargar nuevamente"}
@@ -73,9 +90,12 @@ async def index(response:Response, db: Session = Depends(get_db)):
 
 @app.get("/api/v1/vehicles/{id}")
 async def index(id, response:Response, db: Session = Depends(get_db)):
-	print(dir( db.query(Vehicles)))
-	data_vehicles_filter = db.query(Vehicles).where(Vehicles.vehicle_id==id)
-	print(data_vehicles_filter)
+	data_vehicles_filter = db.query(Vehicles).filter(Vehicles.vehicle_id==id).all()
+	response_data = {"vehicle_id": data_vehicles_filter[0].vehicle_id}
+	response.status_code == 200
+	return response_data
+	
+	
 	
 	return {"message":"Desde el 2 endpoint"}
 
