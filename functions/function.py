51d7366b3774314cf1282e826ed663a85a281d04
Rojas-Andres <anferoja@bancolombia.com.co>
2022-05-12 12:58:43 -0500

    proyecto delegaciones

---
+++ functions/function.py
@@ -0,0 +1,35 @@
+import requests
+from bs4 import BeautifulSoup
+
+def obtener_datos_delegaciones():
+	lista_url = obtener_url()
+	datos = obtener_data_delegados(lista_url)
+	return datos 
+
+def obtener_url():
+	r = requests.get('https://www.archivo.cdmx.gob.mx/gobierno/delegaciones')
+	lista = []
+	if r.status_code == 200:
+		soup = BeautifulSoup(r.content,'html.parser')
+		datos = soup.find_all('div',attrs={"class":"Panel-accordion--simple"})
+		for i in datos:
+			d = i.find('div',attrs={"class":"Link-viewfull"}).find('a').get('href')
+			url = f"https://www.archivo.cdmx.gob.mx/{d}"
+			lista.append(url)  
+	return lista
+
+def obtener_data_delegados(lista):
+	data = []
+	for i in lista:
+		dic = {}
+		r = requests.get(i)
+		soup = BeautifulSoup(r.content,'html.parser')
+		codigo_postal = soup.find_all('div',attrs={"class":"Panel-credential"})[1].find_all('div',attrs={"class":"Panel-credential-data"})[3].text.replace("Código Postal:","").strip().rstrip().split("–")
+		codigo_postal = [i.strip().rstrip() for i in codigo_postal]
+		dic["delegacion"] = soup.find('div',attrs={"class":"Title-0"}).text.strip().rstrip()
+		dic["alcalde"] = soup.find('div',attrs={"class":"Panel-credential-holder"}).text.replace("Titular:","").replace("\n","").strip().rstrip()
+		dic["codigo_inicial"] = codigo_postal[0]
+		dic["codigo_final"] = codigo_postal[1]		
+		data.append(dic)
+	return data 
+		
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Response, Depends
 from psycopg2 import OperationalError
+from bs4 import BeautifulSoup
 from core.config import settings
 from sqlalchemy.orm import Session
 from db.session import get_db, engine 
@@ -13,13 +14,12 @@ import random
 import hashlib
 import os
 import uvicorn
-
+from functions.function import *
 def create_tables():           
 	Base.metadata.create_all(bind=engine)
 
-
-
 app = FastAPI(title=settings.PROJECT_NAME,version=settings.PROJECT_VERSION)
+# app = FastAPI()
 create_tables()      
 	
 def insert_data(statistics, db):
@@ -39,10 +39,14 @@ def insert_data(statistics, db):
 
 
 @app.get("/")
-async def index(response:Response, db: Session = Depends(get_db)):
+async def index(response:Response):#, db: Session = Depends(get_db)):
 	  return {"message":"hola mundo"}
 
-
+@app.get("/ruta")
+async def datos_delegaciones(response:Response):
+	datos = obtener_datos_delegaciones()
+	print(datos)
+	return ""
 if __name__=="__main__":
 	PORT = int(os.environ.get('PORT', 8000))
 	uvicorn.run("main:app",host='0.0.0.0',port=PORT ,reload=True)
