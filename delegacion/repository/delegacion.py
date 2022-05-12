b11d60e90a2726fa33428293a86c10f9948aee68
Rojas-Andres <anferoja@bancolombia.com.co>
2022-05-12 13:35:57 -0500

    Delete delegacion

--- delegacion/repository/delegacion.py
+++ delegacion/repository/delegacion.py
@@ -3,7 +3,11 @@ from db.models.statistics import Delegaciones
 from fastapi import HTTPException,status
 import requests
 
+def delete_all_delegacion(db:Session):
+    db.query(Delegaciones).delete()
+    db.commit()
 def insert_delegacion(datos,db:Session):
+    delete_all_delegacion(db)
     for i in datos:
         new_delegacion = Delegaciones(delegacion=i["delegacion"],titulo="alcalde",nombre=i["alcalde"],codigo_postal_inicial=i["codigo_inicial"],codigo_postal_final=i["codigo_final"])
         db.add(new_delegacion)
